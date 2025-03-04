c3fd3772c37524f096a0069d5013dc0855a6d6e8
Iakov Nakhimovski <iakov.nakhimovski@modelon.com>
2025-03-04 11:28:00 +0000

    fix: added download of artifacts and cmake install as a part of setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 set -euo pipefail
 
+ARTIFACTS_URL=https://github.com/modelon-community/semla-license-manager-impact-example/releases/download/1.0.0-beta.1
+
+SEMLA_OPENSSL_ZIP=SemlaOpenSSL-1.1.0-linux64-gcc485.zip
+SEMLA_LIBJWT_ZIP=SemlaLibJWT-1.0.14-linux64-gcc485.zip
+JANSSON_ZIP=jansson-1.0.4.zip
+JWK2KEY=jwk2key
+
+for ART in ${SEMLA_OPENSSL_ZIP} ${SEMLA_LIBJWT_ZIP} ${JANSSON_ZIP} ${JWK2KEY}
+do
+    if [ ! -f ${ART} ]; then
+        echo Downloading ${ART}
+        curl -LO ${ARTIFACTS_URL}/${ART}
+    fi
+done
+chmod +x ${JWK2KEY}
+
 if [ ! -d ../SEMLA ]; then
     echo "Cloning SEMLA to ../SEMLA"
     git clone https://github.com/modelica/Encryption-and-Licensing.git ../SEMLA
@@ -18,3 +34,23 @@ if [ ! -d ../openssl_keys ]; then
         echo public_key_tool.pem > public_key_tools.txt 
     )
 fi
+
+if [ ! -f jwt_keys/public_keys_jwt.txt ]; then
+    ./update_jwt_keys_from_wellknown.sh
+fi
+
+mkdir -p ve
+if ! command -v cmake 2>&1 >/dev/null
+then
+    echo "cmake not found; setting up in a venv"
+    if [ ! -d build/venv]; then
+        echo Creating Python venv and installing cmake
+        python -m venv build/venv
+        . build/venv/bin/activate
+        pip install cmake
+    fi
+fi
+
+# if [ ! -f /usr/include/check.h ] ; then
+#  TODO: disable check
+# fi
